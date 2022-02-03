43d2d65bcc65d1b0475ebc6a72b1b41914314a5c
Lucas Bramlage <lucas.bramlage@gmail.com>
2022-02-04 00:25:54 +0100

    Split modules and config

---
+++ apps/modules/InputSelect.py
@@ -0,0 +1,158 @@
+import hassapi as hass
+
+
+class InputSelect(hass.Hass):
+    def initialize(self):
+        # Define self variables
+        self.handle = None
+        self.Utils = self.get_app("utils")
+        self.entity = self.args["entity"]
+
+        # Set up triggers
+        if "triggers" in self.args:
+            for trigger in self.args["triggers"]:
+                if "entity" in trigger:
+                    self.listen_state(self.entity_callback, trigger["entity"], config=trigger)
+                elif "time" in trigger:
+                    self.run_daily(self.time_callback, trigger["time"], config=trigger)
+                elif "event" in trigger:
+                    self.listen_event(self.event_callback, "deconz_event", id=trigger["event"], config=trigger)
+
+        # Listen to input_select.
+        self.listen_state(self.set_entities, self.entity)
+
+        # Listen to charging entity.
+        if "charger_entity" in self.args:
+            self.listen_state(self.is_charging, self.args["charger_entity"])
+            self.listen_event(self.set_awake, "mobile_app_notification_action", action="yes")
+
+    # Callbacks
+    def set_awake(self, event, data, kwargs):
+        if "tag" in data:
+            if data["tag"].split(" ")[0] == self.entity:
+                self.select_option(self.entity, "Awake")
+
+    def is_charging(self, entity, attribute, old, new, kwargs):
+        if self.get_state(self.args["entity"]) == "Sleeping":
+            if new == "none":
+                identifier = self.args["entity"] + " > awake"
+                self.call_service(
+                    f"notify/{self.args['mobile_app']}",
+                    message="Are you awake?",
+                    data={
+                        "tag": identifier,
+                        "actions": [{"action": "yes", "title": "Yes"}, {"action": "no", "title": "No"}],
+                    },
+                )
+
+    def entity_callback(self, entity, attribute, old, new, kwargs):
+        try:
+            self.run_trigger(kwargs["config"])
+        except Exception as e:
+            message = f"Trigger failed for {self.entity} with error: {e}"
+            self.call_service("notify/persistent_notification", message=message)
+
+    def event_callback(self, event, data, kwargs):
+        self.log(data["event"])
+        if data["event"] == kwargs["config"]["state"]:
+            try:
+                self.run_trigger(kwargs["config"])
+            except Exception as e:
+                message = f"Trigger failed for {self.entity} with error: {e}"
+                self.call_service("notify/persistent_notification", message=message)
+
+    def time_callback(self, kwargs):
+        try:
+            self.run_trigger(kwargs["config"])
+        except Exception as e:
+            message = f"Trigger failed for {self.entity} with error: {e}"
+            self.call_service("notify/persistent_notification", message=message)
+
+    def set_entities(self, entity, attribute, old, new, kwargs):
+        if "automation_boolean" in self.args:
+            if self.get_state(self.args["automation_boolean"]) == "off":
+                return
+
+        self.start_timeout(self.get_state(self.entity))
+        if "states" in self.args:
+            if new in self.args["states"]:
+                self.Utils.set_entities(self.args["states"], new)
+        if new == "Awake":
+            self.run_in(self.set_home, 5)
+        elif new == "Disabled" and self.handle != None:
+            self.cancel_timer(self.handle)
+
+    def set_home(self, kwargs):
+        self.call_service("input_select/select_option", entity_id=self.entity, option="Home")
+
+    def reset_to_idle(self, kwargs):
+        motion_list = []
+        for trigger in self.args["triggers"]:
+            if "entity" in trigger:
+                state = self.get_state(trigger["entity"], attribute="all")
+                if "device_class" in state["attributes"]:
+                    if state["attributes"]["device_class"] == "motion":
+                        if state["state"] == "on":
+                            motion_list.append(True)
+                        else:
+                            motion_list.append(False)
+        if True not in motion_list:
+            self.call_service("input_select/select_option", entity_id=self.entity, option="Idle")
+        else:
+            if self.handle != None:
+                self.cancel_timer(self.handle)
+            self.log(f"Trigger state still `on` for {self.entity} looping.")
+            self.handle = self.run_in(
+                self.reset_to_idle,
+                kwargs["time"],
+                time=kwargs["time"],
+            )
+
+    # Functions
+    def run_trigger(self, config):
+        # Generate a binary list of condition states
+        state_list = []
+        if "condition" in config:
+            for condition in config["condition"]:
+                if "state" in condition:
+                    if self.get_state(condition["entity"]) == condition["state"]:
+                        state_list.append(True)
+                    else:
+                        state_list.append(False)
+                elif "expression" in condition:
+                    entity_state = self.get_state(condition["entity"])
+                    entity_state = f'"{entity_state}"' if not self.Utils.is_float(entity_state) else entity_state
+                    if eval(f'{entity_state} {condition["expression"]}'):
+                        state_list.append(True)
+                    else:
+                        state_list.append(False)
+
+        # Check condition state list
+        if False not in state_list:
+            if "entity" in config:
+                state = self.get_state(config["entity"])
+                if "state" in config:
+                    if config["state"] == state:
+                        self.set_area(config)
+                elif "expression" in config:
+                    state = f'"{state}"' if not self.Utils.is_float(state) else state
+                    if eval(f'{state} {config["expression"]}'):
+                        self.set_area(config)
+            else:
+                self.set_area(config)
+
+    def set_area(self, config):
+        self.call_service(
+            "input_select/select_option",
+            entity_id=self.entity,
+            option=config["set_state"],
+        )
+
+    def start_timeout(self, state):
+        if "timeout" in self.args.keys():
+            if state in self.args["timeout"]:
+                delay = self.args["timeout"][state]
+                if self.handle != None:
+                    self.cancel_timer(self.handle)
+                self.log(f"Resetting {self.entity} to Idle in {delay} seconds.")
+                self.handle = self.run_in(self.reset_to_idle, delay, time=delay)
---
+++ apps/modules/MediaPlayer.py
@@ -0,0 +1,61 @@
+import hassapi as hass
+
+
+class MediaPlayer(hass.Hass):
+    def initialize(self):
+        self.entity = self.args["entity"]
+        self.linked = self.args["linked"] if "linked" in self.args.keys() else None
+        self.adjust_volume = (
+            self.args["adjust_volume"] if "adjust_volume" in self.args.keys() else None
+        )
+        self.remote_device = (
+            self.args["remote_device"] if "remote_device" in self.args.keys() else None
+        )
+        self.listen_state(self.changed, self.entity)
+        if self.adjust_volume:
+            self.run_daily(self.set_volume, "23:00:00", volume=0.2)
+            self.run_daily(self.set_volume, "9:00:00", volume=0.4)
+        if "set_volume" in self.args:
+            self.listen_state(self.set_start_volume, self.entity, volume=self.args["set_volume"])
+
+    def changed(self, entity, attribute, old, new, kwargs):
+        if new == "off":
+            self.run_in(self.turn_off_linked, 300)
+        elif new == "playing":
+            self.turn_on_linked()
+
+    def set_start_volume(self, entity, attribute, old, new, kwargs):
+        if new == "on":
+            self.call_service("media_player/volume_set", entity_id=self.args["entity"], volume_level=kwargs["volume"])
+
+    def turn_off_linked(self, kwargs):
+        player_state = self.get_state(entity_id=self.entity)
+        if player_state == "off":
+            if self.remote_device:
+                self.call_service(
+                    "remote/send_command",
+                    entity_id=self.linked,
+                    device=self.remote_device,
+                    command="PowerOff",
+                )
+            else:
+                self.call_service("media_player/turn_off", entity_id=self.linked)
+            self.log(f"{self.linked} turned off.")
+        else:
+            self.log(f"Ignoring {self.entity}, not off.")
+
+    def turn_on_linked(self):
+        if self.remote_device:
+            self.call_service(
+                "remote/send_command",
+                entity_id=self.linked,
+                device=self.remote_device,
+                command="PowerOn",
+            )
+
+    def set_volume(self, kwargs):
+        volume = kwargs["volume"]
+        self.call_service(
+            "media_player/volume_set", entity_id=self.entity, volume_level=volume
+        )
+        self.log(f"{self.entity} volume set to {volume}.")
